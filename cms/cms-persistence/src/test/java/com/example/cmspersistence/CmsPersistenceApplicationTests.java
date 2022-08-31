0c16f489aa168fded7416b93f271abfa59267eee
ShaiqHuseynzadeSetronica <s.huseynzade@arammeem.com>
2022-08-31 07:23:16 +0400

    cms project setting

---
+++ cms/cms-persistence/src/test/java/com/example/cmspersistence/CmsPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmspersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cms/cms-web/src/test/java/com/example/cmsweb/CmsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
